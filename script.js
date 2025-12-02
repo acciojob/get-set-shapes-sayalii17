4cf8c18246dd795b5658f84ffd48ec8b5d2c0a0f
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2025-12-02 19:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this.width= width;
+		this.height= height;
+	}
+	width(){
+		console.log(this.width);
+	}
+	height(){
+		console.log(this.height);
+	}
+	getArea(){
+		console.log(width*height);
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle{
+	constructor(side){
+		this.side= side;
+	}
+	getArea(side){
+		console.log(side*side);
+	}
+	getPerimeter(){
+		console.log(`4 * ${this.side}`);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
